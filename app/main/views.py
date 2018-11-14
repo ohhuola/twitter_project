a1167c3de7afd20351410de54a60bec3ef1966b9
ohhuola <wuyixin991014@163.com>
2018-11-14 17:46:07 +0800

    增加了common_adduser()和common_manage()两个函数,以及对他们的调用

--- app/main/views.py
+++ app/main/views.py
@@ -13,8 +13,8 @@ import math
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 from app import utils
-from app.models import CfgNotify
-from app.main.forms import CfgNotifyForm
+from app.models import CfgNotify,Twitter
+from app.main.forms import CfgNotifyForm,UserForm
 from . import main
 
 logger = get_logger(__name__)
@@ -47,10 +47,34 @@ def common_list(DynamicModel, view):
             'total_page': math.ceil(total_count / length), 'page': page, 'length': length}
     return render_template(view, form=dict, current_user=current_user)
 
+def common_manage(DynamicModel,view):
+    # 接收参数
+    action = request.args.get('action')
+    id = request.args.get('id')
+    page = int(request.args.get('page')) if request.args.get('page') else 1
+    length = int(request.args.get('length')) if request.args.get('length') else cfg.ITEMS_PER_PAGE
+
+    # 删除操作
+    if action == 'del' and id:
+        try:
+            DynamicModel.get(DynamicModel.id == id).delete_instance()
+            flash('删除成功')
+        except:
+            flash('删除失败')
+
+    # 查询列表
+    query = DynamicModel.select()
+    total_count = query.count()
+
+    # 处理分页
+    if page: query = query.paginate(page, length)
+
+    dict = {'content': utils.query_to_list(query), 'total_count': total_count,
+            'total_page': math.ceil(total_count / length), 'page': page, 'length': length}
+    return render_template(view, form=dict, current_user=current_user)
 
 # 通用单模型查询&新增&修改
 def common_edit(DynamicModel, form, view):
-    print(request.args)
     id = request.args.get('id', '')
     if id:
         # 查询
@@ -76,6 +100,31 @@ def common_edit(DynamicModel, form, view):
             utils.flash_errors(form)
     return render_template(view, form=form, current_user=current_user)
 
+def common_adduser(DynamicModel,form,view):
+    id = request.args.get('id', '')
+    if id:
+        # 查询
+        model = DynamicModel.get(DynamicModel.id == id)
+        if request.method == 'GET':
+            utils.model_to_form(model, form)
+        # 修改
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                utils.form_to_model(form, model)
+                model.save()
+                flash('修改成功')
+            else:
+                utils.flash_errors(form)
+    else:
+        # 新增
+        if form.validate_on_submit():
+            model = DynamicModel()
+            utils.form_to_model(form, model)
+            model.save()
+            flash('保存成功')
+        else:
+            utils.flash_errors(form)
+    return render_template(view, form=form, current_user=current_user)
 
 # 根目录跳转
 @main.route('/', methods=['GET'])
@@ -105,14 +154,23 @@ def index():
                            script_list=map.get_js_dependencies(),
                            current_user=current_user)
 
+#用户管理
+@main.route('/manage',methods=['GET','POST'])
+@login_required
+def manage():
+    return common_manage(Twitter, 'managelist.html')
+#增加用户
+@main.route('/adduser',methods=['GET','POST'])
+@login_required
+def adduser():
+    return common_adduser(Twitter,UserForm(),'Adduser.html')
+
 
 # 通知方式查询
 @main.route('/notifylist', methods=['GET', 'POST'])
 @login_required
 def notifylist():
-    return common_list(CfgNotify, 'notifylist.html')
-
-
+    return common_list(CfgNotify,'notifylist.html')
 # 通知方式配置
 @main.route('/notifyedit', methods=['GET', 'POST'])
 @login_required
