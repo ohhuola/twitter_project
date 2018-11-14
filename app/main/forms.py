2f258c3a9a036a112f12aa40590fa23d39a40e6a
ohhuola <ohhuola@outlook.com>
2018-11-14 23:46:49 +0800

    add searchForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, PasswordField, SelectField, TextAreaField, HiddenField
-from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import StringField, SubmitField, BooleanField,  SelectField
+from wtforms.validators import DataRequired, Length
 
 
 class CfgNotifyForm(FlaskForm):
@@ -19,3 +19,10 @@ class UserForm(FlaskForm):
     recent_active_time=StringField('最近活跃时间',validators=[DataRequired(message='不能为空'),Length(0,200,message='长度不正确')])
     submit = SubmitField('提交')
     # rencet_active_time使用StringField还是DateField有待商榷
+
+class SearchForm(FlaskForm):
+    user_name = StringField('用户名字', validators=[DataRequired(message='不能为空'), Length(0, 200, message='长度不正确')])
+    user_nick = StringField('用户昵称', validators=[Length(0, 200, message='长度不正确')])
+    number_of_tweets = StringField('累计发推数量', validators=[Length(0, 200, message='长度不正确')])
+    recent_active_time = StringField('最近活跃时间', validators=[Length(0, 200, message='长度不正确')])
+    submit = SubmitField('提交')
